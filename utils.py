3439088d74906bc0110437863a62baabda5808b0
ilay menahem <110230889+IlayMenahem@users.noreply.github.com>
2024-11-28 01:46:55 +0200

    bugfix: undefined encoding

--- utils.py
+++ utils.py
@@ -2,9 +2,12 @@ import gymnasium as gym
 import jax.numpy as jnp
 import numpy as np
 
-encoding = {' ': 0, ':': -1, '|': 1, 'G': -2, 'F': 2, 'P': 4, 'T': -4}
+encoding = {' ': 0, ':': -1, '|': 1, 'G': -2, 'F': 2, 'P': 3, 'T': 5}
 def encode(value):
-    return encoding[value]
+    '''
+    sum over the characters in the value encodings
+    '''
+    return sum([encoding[char] for char in value])
 
 def get_passenger_locations(env):
     return [passenger.location for passenger in env.unwrapped.state().passengers]
@@ -12,10 +15,10 @@ def get_passenger_locations(env):
 def get_taxi_location(env):
     return env.unwrapped.state().taxis[0].location
 
-def put_at_location(location, symbol, domain_map):
+def append_at_location(location, symbol, domain_map):
     row, col = location
     col *= 2
-    domain_map[row, col] = symbol
+    domain_map[row, col] += symbol
 
     return domain_map
 
@@ -24,8 +27,8 @@ def get_domain_map(env):
     domain_map = np.array(domain_map[1:-1, 1:-1])
 
     for passenger_location in get_passenger_locations(env):
-        domain_map = put_at_location(passenger_location, 'P', domain_map)
-    domain_map = put_at_location(get_taxi_location(env), 'T', domain_map)
+        domain_map = append_at_location(passenger_location, 'P', domain_map)
+    domain_map = append_at_location(get_taxi_location(env), 'T', domain_map)
 
     return domain_map
 
@@ -36,7 +39,7 @@ class MapWrapper(gym.Wrapper):
 
         self.observation_space = gym.spaces.Dict({
             'symbolic': env.observation_space,
-            'domain_map': gym.spaces.Box(-4, 4, shape=self.prepare_domain_map().shape)
+            'domain_map': gym.spaces.Box(-15, 15, shape=self.prepare_domain_map().shape)
         })
 
     def prepare_domain_map(self):
@@ -69,19 +72,3 @@ class MapWrapper(gym.Wrapper):
         }
 
         return observation, reward, done, truncated, info
-
-class BCWrapper(gym.Wrapper):
-    def __init__(self, env, expert):
-        super().__init__(env)
-        self.expert = expert
-    
-    def step(self, action):
-        observation, _, done, truncated, info = self.env.step(action)
-        expert_action = self.expert(observation)
-
-        if action != expert_action:
-            reward = -1
-        else:
-            reward = 1
-
-        return observation, reward, done, truncated, info        
